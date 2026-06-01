9ac55c2a293e8388168cd1c635cc361246ff0e8b
Kamala Garayeva <kemaleqarayeva61@gmail.com>
2026-06-01 13:46:39 +0400

    Add files via upload

---
+++ mg_level_ttest.R
@@ -0,0 +1,16 @@
+> x=c(22,26,54,34,19,45,65)
+> y=c(26,29,49,30,22,46,60)
+> t.test(x,y,alternative = 'greater')
+
+Welch Two Sample t-test
+
+data:  x and y
+t = 0.05052, df = 11.546, p-value = 0.4803
+alternative hypothesis: true difference in means is greater than 0
+95 percent confidence interval:
+  -14.74072       Inf
+sample estimates:
+  mean of x mean of y 
+37.85714  37.42857 
+
+> 
